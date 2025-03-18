3449fb976dd288f2493f0a3b6e8e6d196b3c72a3
keioni <devel@keioni.com>
2025-03-18 20:35:39 +0900

    add SPEC.md and remove many files

---
+++ nemulo/convert.py
@@ -0,0 +1,11 @@
+"""
+Convert text to HTML.
+
+This module provides functions to convert text to HTML.
+Specifications for this module are defined in the SPEC.md file.
+
+"""
+
+class Convert:
+    def __init__(self):
+        pass
